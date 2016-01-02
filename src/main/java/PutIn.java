a37abc548d52e4d51c2f3169fe996705d34e14ad
mjduijn <m.j.duijn@student.tudelft.nl>
2016-01-02 15:12:07 +0100

    Added operator for mass put

---
+++ src/main/java/PutIn.java
@@ -0,0 +1,62 @@
+import org.iq80.leveldb.DB;
+import rx.Observable.Operator;
+import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class PutIn implements Operator<SnappyrDB, Map.Entry<String, Object>> {
+    SnappyrDB db;
+
+    public PutIn(SnappyrDB db) {
+        this.db = db;
+    }
+
+    @Override
+    public Subscriber<? super Map.Entry<String, Object>> call(final Subscriber<? super SnappyrDB> s) {
+        return new Subscriber<Map.Entry<String, Object>>(s) {
+            @Override
+            public void onCompleted() {
+                if (!s.isUnsubscribed()) {
+                    s.onNext(db); //Only passes on a single DB item
+                    s.onCompleted();
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                if (!s.isUnsubscribed()) {
+                    s.onError(t);
+                }
+            }
+
+            @Override
+            public void onNext(Map.Entry<String, Object> entries) {
+                if (!s.isUnsubscribed()) {
+                    db.query()
+                    .put(entries.getKey(), entries.getValue())
+                            .execute(new Action1<DB>() {
+                                         @Override
+                                         public void call(DB entries) {
+                                             //Do nothing
+                                         }
+                                     },
+                                    new Action1<Throwable>() {
+                                        @Override
+                                        public void call(Throwable throwable) {
+                                            s.onError(throwable);
+                                        }
+                                    },
+                                    new Action0() {
+                                        @Override
+                                        public void call() {
+                                            //Do nothing
+                                        }
+                                    });
+                }
+            }
+        };
+    }
+}
